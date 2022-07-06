54d03ea4a196113aa6958349f14fd7bc765b2ec4
million-art <millionmulugeta01@gmail.com>
2022-07-06 15:58:11 +0300

    commited

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char a='b';
+	puts("\"Programming is like building a multilingual puzzle %c",a);
+	return (0);
+}
